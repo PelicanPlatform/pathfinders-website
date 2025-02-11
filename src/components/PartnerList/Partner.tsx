ebab76fdb9c0395cde79b05de9afa62e1fb436c5
fallow64 <fallow640@gmail.com>
2025-02-11 13:06:44 -0600

    Add learn more link to partners

--- src/components/PartnerList/Partner.tsx
+++ src/components/PartnerList/Partner.tsx
@@ -1,41 +1,20 @@
-import { Box, SxProps } from "@mui/material";
-import Image from "next/image";
-import AwardLink from "../AwardLink";
-import { CSSProperties, ReactNode } from "react";
-import Link from "next/link";
 import { Launch } from "@mui/icons-material";
+import { Box, Link as MUILink, SxProps } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+import AwardLink from "../AwardLink";
 
 const itemContainer: SxProps = {
+  display: "flex",
+  flexDirection: "column",
+
   bgcolor: "primary.main",
   maxWidth: "100%",
 
   padding: "20px",
   borderRadius: "20px",
   boxShadow: 1,
-
-  flexDirection: "column",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
-const logo: CSSProperties = {
-  width: "200px",
-  maxWidth: "100%",
-  height: "auto",
-  objectFit: "contain",
-  flexGrow: 1,
-};
-
-const nameAndAward: CSSProperties = {
-  fontSize: "1.25rem",
-  textAlign: "center",
-  flexShrink: 0,
-};
-
-const nameLink: CSSProperties = {
-  fontStyle: "italic",
-  textDecoration: "underline",
 };
 
 type PartnerImage =
@@ -52,41 +31,65 @@ type PartnerImage =
 type PartnerProps = PartnerImage & {
   area: string;
   name: string;
-  nameLink?: string;
   awardId: string;
-  containerStyles?: SxProps;
+  aboutLink: string;
+  containerSx?: SxProps;
 };
 
 const Partner = (props: PartnerProps) => {
   return (
-    <Box
-      sx={{ ...itemContainer, ...props.containerStyles }}
-      gridArea={props.area}
-    >
-      {"image" in props ? (
-        props.image
-      ) : (
-        <Image
-          style={logo}
-          src={props.src}
-          alt={props.alt}
-          width={props.width}
-          height={props.height}
-        />
-      )}
-      <span style={nameAndAward}>
-        {props.nameLink ? (
-          <Link href={props.nameLink} target="_blank" style={nameLink}>
-            {props.name}
-          </Link>
+    <Box sx={{ ...itemContainer, ...props.containerSx }} gridArea={props.area}>
+      <Box flexGrow={1}>
+        {"image" in props ? (
+          <Box>{props.image}</Box>
         ) : (
-          <span>{props.name}</span>
-        )}{" "}
-        &mdash; <AwardLink id={props.awardId} />
-      </span>
+          <Image
+            style={{
+              maxWidth: "100%",
+              height: "auto",
+              objectFit: "contain",
+            }}
+            src={props.src}
+            alt={props.alt}
+            width={props.width}
+            height={props.height}
+          />
+        )}
+      </Box>
+      <Box>
+        <span
+          style={{
+            fontSize: "1.25rem",
+          }}
+        >
+          <span>
+            {props.name} &mdash; <AwardLink id={props.awardId} />
+          </span>
+          <br />
+        </span>
+
+        <MUILink
+          component={Link}
+          href={props.aboutLink}
+          target="_blank"
+          color="#000"
+          fontSize="1rem"
+          sx={{
+            cursor: "pointer",
+            fontStyle: "italic",
+            textDecoration: "none",
+          }}
+          display="flex"
+          alignItems="center"
+        >
+          <Box component="span" mr="5px">
+            Learn More
+          </Box>
+          <Launch fontSize="small" />
+        </MUILink>
+      </Box>
     </Box>
   );
 };
 
 export default Partner;
-export { itemContainer, logo, nameAndAward };
--- src/components/PartnerList/index.tsx
+++ src/components/PartnerList/index.tsx
@@ -9,7 +9,6 @@ const grid: SxProps = {
 
   gap: "30px",
   gridAutoColumns: "1fr",
-  gridAutoRows: "1fr",
   gridTemplateAreas: {
     xs: `
       "a b"
@@ -17,8 +16,8 @@ const grid: SxProps = {
       "e e"
     `,
     md: `
-      "a a a b b b c c c d d d"
-      ". . . . e e e e . . . ."
+      "a a b b c c d d"
+      ". . e e e e . ."
     `,
   },
 };
@@ -34,16 +33,17 @@ const PartnerList = () => {
         width={518}
         height={142}
         awardId="1852977"
+        aboutLink="https://ncar.ucar.edu/"
       />
       <Partner
         area="b"
         src="/images/logos/pism_logo.png"
         alt="PISM Logo"
         name="PISM"
-        nameLink="https://www.pism.io/"
         width={1000 * 0.5}
         height={332 * 0.5}
         awardId="2324718"
+        aboutLink="https://www.pism.io/"
       />
       <Partner
         area="c"
@@ -53,24 +53,30 @@ const PartnerList = () => {
         width={221.7}
         height={102.8}
         awardId="2322308"
+        aboutLink="https://environment.fiu.edu/"
       />
       <Partner
         area="d"
         name="Sonar AI"
         awardId="2311843"
         image={<SonarAILogo />}
+        aboutLink="/sonar-ai"
       />
       <Partner
         area="e"
-        src="/images/logos/pelican_full_logo.png"
+        src="/images/logos/pelican_logo.png"
         alt="Pelican Logo"
+        width={400 / 2}
+        height={395 / 2}
         name="Pelican"
-        width={500}
-        height={178}
         awardId="2324718"
-        containerStyles={{
+        containerSx={{
+          display: "flex",
           bgcolor: "secondary.main",
+          flexDirection: "row",
+          alignItems: "center",
         }}
+        aboutLink="/pelican"
       />
     </Box>
   );
